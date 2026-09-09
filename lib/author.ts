d76774057f92628c522f8e872c12444262fc99c5
erwinmehehe <erwinvalles20@gmail.com>
2026-09-09 20:47:53 +0800

    Add MotoIndex author identity

---
+++ lib/author.ts
@@ -0,0 +1,14 @@
+import { absoluteUrl } from "@/lib/site";
+
+export const AUTHOR_NAME = "Erwin Valles";
+export const AUTHOR_PATH = "/authors/erwin-valles";
+export const AUTHOR_ROLE = "Author and Editor";
+export const AUTHOR_BIO = "Erwin Valles is the author and editor behind MotoIndex Philippines. He reviews motorcycle prices, specifications, riding gear and buyer guides using traceable manufacturer, government and Philippine market sources.";
+
+export function authorPersonSchema() {
+  return {
+    "@type": "Person",
+    name: AUTHOR_NAME,
+    url: absoluteUrl(AUTHOR_PATH),
+  };
+}
