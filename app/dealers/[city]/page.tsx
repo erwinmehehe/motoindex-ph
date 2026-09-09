1e7e9a458380beda73cd4052f8f495ef38837996
erwinmehehe <erwinvalles20@gmail.com>
2026-09-09 16:21:21 +0800

    Fix shared dealer threshold import

--- app/dealers/[city]/page.tsx
+++ app/dealers/[city]/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import Link from "next/link";
 import { Breadcrumbs } from "@/components/Breadcrumbs";
 import { notFound } from "next/navigation";
-import { MIN_PUBLIC_DEALERS_PER_CITY_PER_CITY, citySlug, publicDealerCities, publicDealersByCity } from "@/lib/sellers";
+import { MIN_PUBLIC_DEALERS_PER_CITY, citySlug, publicDealerCities, publicDealersByCity } from "@/lib/sellers";
 import { pageMetadata } from "@/lib/site";
 
 
