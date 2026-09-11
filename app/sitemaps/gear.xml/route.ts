05fc15fe738cace8f1a1f5c5fbb001c86430e2a9
erwinmehehe <erwinvalles20@gmail.com>
2026-09-11 12:33:30 +0800

    Make gear sitemap static-export compatible

--- app/sitemaps/gear.xml/route.ts
+++ app/sitemaps/gear.xml/route.ts
@@ -1,2 +1,5 @@
 import { gearSitemapEntries, sitemapXml } from "@/lib/sitemaps";
+
+export const dynamic = "force-static";
+
 export function GET(){return new Response(sitemapXml(gearSitemapEntries()),{headers:{"Content-Type":"application/xml; charset=utf-8","Cache-Control":"public, max-age=3600, s-maxage=3600"}});}
