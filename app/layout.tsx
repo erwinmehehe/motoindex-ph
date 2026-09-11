4440358c001d8ea1cb1c97d0dfbc93c0d52cb1ea
erwinmehehe <erwinvalles20@gmail.com>
2026-09-11 23:29:40 +0800

    Preserve existing website search schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,7 +77,7 @@ const websiteSchema = {
   potentialAction: {
     "@type": "SearchAction",
     target: `${SITE_URL}/search?q={search_term_string}`,
-    "query-input": "required name=query-input"
+    "query-input": "required name=search_term_string"
   }
 };
 
