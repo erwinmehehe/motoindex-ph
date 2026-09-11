159792f795162f736257534263da107fb531573d
erwinmehehe <erwinvalles20@gmail.com>
2026-09-11 13:04:55 +0800

    Remove server redirect route from static export

--- app/used-motorcycles/[make]/[slug]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { permanentRedirect } from "next/navigation";
-
-export default async function ConsolidatedUsedModelRoute({ params }: { params: Promise<{ make: string; slug: string }> }) {
-  const { make, slug } = await params;
-  permanentRedirect(`/motorcycles/${make}/${slug}#used`);
-}
