a6d5eaa36af79379089722dd18607c6ef93944d8
erwinmehehe <erwinvalles20@gmail.com>
2026-09-11 12:59:25 +0800

    Remove server redirect route from static export

--- app/fitment/[make]/[slug]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { permanentRedirect } from "next/navigation";
-
-export default async function ConsolidatedFitmentRoute({ params }: { params: Promise<{ make: string; slug: string }> }) {
-  const { make, slug } = await params;
-  permanentRedirect(`/motorcycles/${make}/${slug}#tires-fitment`);
-}
