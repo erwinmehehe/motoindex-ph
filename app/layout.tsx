13322082d7395cfa3fa8c5b460cc84c6a52f3075
erwinmehehe <erwinvalles20@gmail.com>
2026-09-12 16:34:06 +0800

    Load hardening UI layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import "./redesign-fonts.css";
 import "./redesign-v2.css";
 import "./arena-sitewide.css";
 import "./responsive-polish.css";
+import "./hardening.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { JsonLd } from "@/components/JsonLd";
