9a0fa27ef700afc59fdefc5e1c2520d098b79fe5
erwinmehehe <erwinvalles20@gmail.com>
2026-09-13 19:44:04 +0800

    Load compact homepage styles last

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ import "./public-hubs-v2.css";
 import "./fitment-v2.css";
 import "./used-v2.css";
 import "./model-fallback-fixes.css";
+import "./homepage-compact-modern.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { JsonLd } from "@/components/JsonLd";
