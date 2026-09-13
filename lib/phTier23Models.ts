c6bbdd29bda723cd2163c118e7e4a087d8af4335
erwinmehehe <erwinvalles20@gmail.com>
2026-09-13 13:04:17 +0800

    Load verified 2026 brand expansion

--- lib/phTier23Models.ts
+++ lib/phTier23Models.ts
@@ -1,4 +1,5 @@
 import { phTier23Motorcycles as baseMotorcycles } from "./phTier23ModelsBase";
 import { phTier23Expansion2026 } from "./phTier23ModelsExpansion2026";
+import { phBrandExpansion2026 } from "./phBrandExpansion2026";
 
-export const phTier23Motorcycles = [...baseMotorcycles, ...phTier23Expansion2026];
+export const phTier23Motorcycles = [...baseMotorcycles, ...phTier23Expansion2026, ...phBrandExpansion2026];
