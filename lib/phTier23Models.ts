89d5bea4efb343bdbf6f5d53e1e38593b6d8fd88
erwinmehehe <erwinvalles20@gmail.com>
2026-09-13 13:09:44 +0800

    Load verified brand coverage expansion

--- lib/phTier23Models.ts
+++ lib/phTier23Models.ts
@@ -1,5 +1,11 @@
 import { phTier23Motorcycles as baseMotorcycles } from "./phTier23ModelsBase";
 import { phTier23Expansion2026 } from "./phTier23ModelsExpansion2026";
 import { phBrandExpansion2026 } from "./phBrandExpansion2026";
+import { phCoverageExpansion2026 } from "./phCoverageExpansion2026";
 
-export const phTier23Motorcycles = [...baseMotorcycles, ...phTier23Expansion2026, ...phBrandExpansion2026];
+export const phTier23Motorcycles = [
+  ...baseMotorcycles,
+  ...phTier23Expansion2026,
+  ...phBrandExpansion2026,
+  ...phCoverageExpansion2026,
+];
