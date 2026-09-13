2f6be3d41d936d1d0cb5dd153ffd8c8619c0cb64
erwinmehehe <erwinvalles20@gmail.com>
2026-09-13 11:11:43 +0800

    Load final experience fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ import "./responsive-polish.css";
 import "./hardening.css";
 import "./arena-premium-v2.css";
 import "./experience-v4.css";
+import "./experience-v4-fixes.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { JsonLd } from "@/components/JsonLd";
