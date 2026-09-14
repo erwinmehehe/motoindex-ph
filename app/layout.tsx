a0ee23290dd71edf9b01a8dc4a7492a43d68a8a6
erwinmehehe <erwinvalles20@gmail.com>
2026-09-14 12:40:10 +0800

    Load compact homepage gear fixes last

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ import "./premium-light-sticky-fix.css";
 import "./premium-light-qa-fixes.css";
 import "./premium-light-final-fixes.css";
 import "./compact-polish-v1.css";
+import "./compact-polish-v1-fixes.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { JsonLd } from "@/components/JsonLd";
