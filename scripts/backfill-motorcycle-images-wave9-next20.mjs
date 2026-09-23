f48b654973439de2d563671d2fdd30f14751ed2f
erwinmehehe <erwinvalles20@gmail.com>
2026-09-23 22:04:39 +0800

    Fix final next-20 motorcycle image sources

--- scripts/backfill-motorcycle-images-wave9-next20.mjs
+++ scripts/backfill-motorcycle-images-wave9-next20.mjs
@@ -27,7 +27,7 @@ const targets = [
 ["husqvarna-norden-901","https://www.husqvarna-motorcycles.com/en-ph/models/travel/norden-901-2022.html",["norden","901"],"Husqvarna Norden 901 adventure motorcycle","Husqvarna Motorcycles"],
 ["husqvarna-svartpilen-200","https://www.husqvarna-motorcycles.com/en-ph/models/naked/svartpilen/svartpilen-200-2023.html",["svartpilen","200"],"Husqvarna Svartpilen 200 motorcycle","Husqvarna Motorcycles"],
 ["kawasaki-ninja-zx-25r","https://kawasakileisurebikes.ph/motorcycles/supersports/ninja-zx-25r-standard/",["ninja","zx-25r"],"Kawasaki Ninja ZX-25R motorcycle","Kawasaki Motors Philippines"],
-["kymco-dollar-150","https://kymco.com.ph/product/dollar-150/",["dollar","150"],"KYMCO Dollar 150 scooter","KYMCO Philippines"],
+["keeway-cafe-racer-152","https://www.keeway.com/ph-en/products/cafe-racer-152",["cafe","racer","152"],"Keeway Cafe Racer 152 motorcycle","Keeway"],
 ["kymco-dink-r-150","https://kymco.com.ph/product/dink-r-150/",["dink","150"],"KYMCO Dink R 150 scooter","KYMCO Philippines"],
 ["kymco-dtx360-300","https://kymco.com.ph/product/dtx-360-300/",["dtx","360"],"KYMCO DTX360-300 scooter","KYMCO Philippines"],
 ["royal-enfield-shotgun-650","https://www.royalenfield.com/ph/en/motorcycles/shotgun-650/",["shotgun","650"],"Royal Enfield Shotgun 650 motorcycle","Royal Enfield"],
@@ -35,14 +35,13 @@ const targets = [
 ].map(([entityId,pageUrl,terms,alt,rightsHolder]) => ({entityId,pageUrl,terms,alt,rightsHolder}));
 
 const directImages = {
-  "bajaj-pulsar-n125": {url:"https://cdn.bajajauto.com/-/media/images/bajajauto/bajaj-price-page/header-drop-down/webp/pulasr-n125.webp"},
+  "bajaj-pulsar-n125": {url:"https://www.bajajauto.com/corporate/media-centre/press-releases/-/media/Images/bajajauto/press-release/Purple-Fury.png", pageUrl:"https://www.bajajauto.com/corporate/media-centre/press-releases/baja-auto-launches-pulsar-n125-the-most-youthful-pulsar-yet"},
   "bajaj-pulsar-ns400z": {url:"https://cdn.bajajauto.com/-/media/assets/bajajauto/bikes/web-header-navigator-images/pulsar-ns400z.webp"},
   "bajaj-pulsar-rs200": {url:"https://cdn.bajajauto.com/-/media/assets/bajajauto/bikes/web-header-navigator-images/pulsar-rs200.webp"},
   "kawasaki-ninja-zx-25r": {
     url:"https://global.kawasaki.com/en/corp/newsroom/news/images/news_191023-Ninja%20ZX-25R.jpg",
     pageUrl:"https://global.kawasaki.com/en/corp/newsroom/news/detail/?f=20191023_0946"
   },
-  "kymco-dollar-150": {url:"https://kymco.com.ph/wp-content/uploads/2025/04/Dollar-Flat-Black.png.webp"},
   "kymco-dink-r-150": {url:"https://kymco.com.ph/wp-content/uploads/2024/02/PL_COL11-2-700x467.png"},
   "kymco-dtx360-300": {url:"https://kymco.com.ph/wp-content/uploads/2024/02/DTX-300cc_1.png"}
 };
